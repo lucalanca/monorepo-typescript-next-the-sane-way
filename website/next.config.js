d6476c60d4bf6929d37681b6cf3453df9b85bc3c
João Figueiredo <lucalanca2@hotmail.com>
2018-12-11 17:32:31 +0100

    Adds bundle stats via next-bundle-analyzer

--- website/next.config.js
+++ website/next.config.js
@@ -1,22 +1,38 @@
+const withPlugins = require('next-compose-plugins');
 const withTypescript = require('@zeit/next-typescript');
 const withTM = require('next-plugin-transpile-modules');
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
-module.exports = withTypescript(
-  withTM({
-    transpileModules: ['shared'],
-    webpack(config, options) {
-      if (options.isServer && options.dev) {
-        const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+module.exports = withPlugins([
+  [withTypescript],
+  [withTM],
+  [withBundleAnalyzer]
+], {
+  transpileModules: ['shared'],
+  webpack(config, options) {
+    if (options.isServer && options.dev) {
+      const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-        config.plugins.push(
-          new ForkTsCheckerWebpackPlugin({
-            tsconfig: '../tsconfig.json',
-            tslint: '../tslint.json',
-          }),
-        );
-      }
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          tsconfig: '../tsconfig.json',
+          tslint: '../tslint.json',
+        }),
+      );
+    }
 
-      return config;
+    return config;
+  },
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
     },
-  })
-);
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
+    }
+  }
+})
